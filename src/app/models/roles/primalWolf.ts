95cd9e4aa32e0575adb4485c4ce932e31bc6010c
Alex <42290929+AlexRomberg@users.noreply.github.com>
2024-10-28 09:04:55 +0100

    added primal wolf functionality

--- src/app/models/roles/primalWolf.ts
+++ src/app/models/roles/primalWolf.ts
@@ -1,12 +1,17 @@
+import { GameStateService } from "../../services/game-state.service";
 import { Action, CirclePerson, Role } from "../../types";
 import { RequestAssignment } from "../actions/buttons";
+import { BigWolf } from "./bigWolf";
 import { BasePriority } from "./roles";
+import { Werewolf } from "./werewolf";
+import { WhiteWolf } from "./whiteWolf";
 
 export class PrimalWolf implements Role, Action {
     public Priority = BasePriority.Wolf + 3;
     public Image = "primal_wolf";
     public Name = "Der Urwolf";
     public AssignedPerson?: CirclePerson | undefined;
+    private isDone = false;
 
     GetPoints = () => [!this.AssignedPerson && "Person zuweisen", "Kann Opfer zu Werwolf machen"];
     GetButtons = () => {
@@ -14,8 +19,22 @@ export class PrimalWolf implements Role, Action {
         if (!this.AssignedPerson) {
             buttons.push(RequestAssignment(this));
         }
-        // TODO: Add victim transformation
+        if (!this.isDone) {
+            buttons.push({
+                title: "Opfer zu Werwolf verwandeln",
+                action: this.RequestVictimStateChangePerson.bind(this),
+            });
+        }
         return buttons;
     };
-    IsAwakeThisNight = () => true; // TODO: Add dead wolf check
+    IsAwakeThisNight = (_: number, gameState: GameStateService) => !this.isDone && gameState.People.filter(p => p.isDead && (p.role instanceof Werewolf || p.role instanceof WhiteWolf || p.role instanceof BigWolf || p.isWerewolf)).length == 0;
+
+    private async RequestVictimStateChangePerson({ gameState }: { gameState: GameStateService }) {
+        const victim = gameState.People.find(p => p.isVictim);
+        if (victim) {
+            victim.isVictim = false;
+            victim.isWerewolf = true;
+            this.isDone = true;
+        }
+    }
 }
