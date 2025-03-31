f476183f276f52e6e7a4b3fc13175206b7c36edc
Alex <42290929+AlexRomberg@users.noreply.github.com>
2025-03-31 13:29:42 +0200

    added service worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, isDevMode } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
@@ -6,11 +6,15 @@ import { provideHttpClient } from "@angular/common/http";
 import { LUCIDE_ICONS, LucideIconProvider } from 'lucide-angular';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { icons } from "./util/icons";
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideHttpClient(), {
-        provide: LUCIDE_ICONS,
-        multi: true,
-        useValue: new LucideIconProvider(icons)
-    }, provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideHttpClient(), {
+    provide: LUCIDE_ICONS,
+    multi: true,
+    useValue: new LucideIconProvider(icons)
+  }, provideAnimationsAsync(), provideServiceWorker('ngsw-worker.js', {
+    enabled: !isDevMode(),
+    registrationStrategy: 'registerWhenStable:30000'
+  })]
 };
